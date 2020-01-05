c2677758699902c6121db331c5e70385d0a9158f
ikechuku <rockstarik2@gmail.com>
2020-01-05 05:48:51 +0100

    Refactor FourColGrid component

--- src/components/elements/FourColGrid/index.js
+++ src/components/elements/FourColGrid/index.js
@@ -1,30 +1,25 @@
-import React from 'react'
-import './FourColGrid.css'
+ import React from "react";
+import "./FourColGrid.css";
 
-const FourColGrid = (props) => { 
-
-const renderElements = ()=>{
-    const gridElements = props.children.map((element, i)=>{
-        return (
-            <div key={i} className="rmdb-grid-element">
-                {element}
-            </div>
-        )
-    })
-    return gridElements
-    
-}
-
-
-    return (
-        <div className='rmdb-grid'>
-            {props.header & !props.loading ? <h1>{props.header}</h1>: null}
-
-            <div className="rmdb-grid-content">
-                {renderElements()}
-            </div>
+const FourColGrid = props => {
+  const renderElements = () => {
+    const gridElements = props.children.map((element, i) => {
+      return (
+        <div key={i} className="rmdb-grid-element">
+          {element}
         </div>
-    )
-}
+      );
+    });
+    return gridElements;
+  };
 
-export default FourColGrid
+  return (
+    <div className="rmdb-grid">
+      {props.header & !props.loading ? <h1>{props.header}Ahead</h1> : null}
+
+      <div className="rmdb-grid-content">{renderElements()}</div>
+    </div>
+  );
+};
+
+export default FourColGrid;
